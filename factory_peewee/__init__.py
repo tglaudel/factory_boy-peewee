6e5ecef330d17fd2cf81e61221b4775f6d57cc26
tglaudel <thomas.glaudel@dolead.com>
2019-03-05 09:34:10 +0100

    Update _setup_next_sequence when compositeKey is required

--- factory_peewee/__init__.py
+++ factory_peewee/__init__.py
@@ -26,6 +26,8 @@ class PeeweeModelFactory(base.Factory):
         db = cls._meta.database
         model = cls._meta.model
         pk = getattr(model, model._meta.primary_key.name)
+        if isinstance(pk, peewee.CompositeKey):
+            return 0
         max_pk = (model.select(peewee.fn.Max(pk).alias('maxpk'))
                        .limit(1).order_by().execute())
         max_pk = [mp.maxpk for mp in max_pk][0]
